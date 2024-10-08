39799a61240f8f4046dc7f1002afafa3a94411ff
lancelot-thomas <121886258+lancelot-thomas@users.noreply.github.com>
2024-10-08 07:19:40 +0000

    asciiart

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""Permet d'encoder une chaine ascii"""
 #### Imports et définition des variables globales
 
 
@@ -5,7 +6,8 @@
 
 
 def artcode_i(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme itératif
+    """retourne la liste de tuples encodant une chaîne de caractères passée 
+    en argument selon un algorithme itératif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -13,14 +15,25 @@ def artcode_i(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
-    # votre code ici
 
-    return [ ]
+    # votre code ici
+    encode = []
+    cmpt = 1
+    for i in range(1, len(s)+1):
+        if i == len(s):
+            encode.append((s[i-1], cmpt))
+            break
+        if s[i] == s[i-1]:
+            cmpt += 1
+        else :
+            encode.append((s[i-1], cmpt))
+            cmpt = 1
+    return encode
 
 
 def artcode_r(s):
-    """retourne la liste de tuples encodant une chaîne de caractères passée en argument selon un algorithme récursif
+    """retourne la liste de tuples encodant une chaîne de caractères 
+    passée en argument selon un algorithme récursif
 
     Args:
         s (str): la chaîne de caractères à encoder
@@ -28,20 +41,37 @@ def artcode_r(s):
     Returns:
         list: la liste des tuples (caractère, nombre d'occurences)
     """
-    
     # votre code ici
 
     # cas de base
     # recherche nombre de caractères identiques au premier
     # appel récursif
 
-    return []
-    
+    if len(s) > 250 :
+        s1 = artcode_r(s[:250])
+        s2 = artcode_r(s[250:])
+        if s1 and s2 and s1[-1][0] == s2[0][0]:
+            s1[-1] = (s1[-1][0], s1[-1][1] + s2[0][1])
+            s2 = s2[1:]
+        return s1 + s2
+
+    if s == "":
+        return []
+    if len(s) == 1:
+        return [(s, 1)]
+    encode = artcode_r(s[1:])
+    if encode[0][0] == s[0]:
+        return [(s[0], encode[0][1]+1)] + encode[1:]
+
+    return [(s[0], 1)] + encode
 
 #### Fonction principale
 
 
 def main():
+    """
+    main appelant les fonctions d'encodage
+    """
     print(artcode_i('MMMMaaacXolloMM'))
     print(artcode_r('MMMMaaacXolloMM'))
 
